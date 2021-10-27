90a7b1956558997c4ed189e183eb14d3e6ecd2d6
lilu mirotadze <mirotadzelilu@yahoo.com>
2021-10-27 19:15:58 +0400

    update

--- javaskript/java.js
+++ javaskript/java.js
@@ -1,31 +1,31 @@
-// let  user = {
-//     firstname: 'giorgi',
-//     lastname: 'smith',
-//     age: 25,
-//     studentstatus:'active'
-// };
-// console.log(user.studentstatus);
+let  user = {
+    firstname: 'giorgi',
+    lastname: 'smith',
+    age: 25,
+    studentstatus:'active'
+}
+console.log(user.studentstatus);
 
 
-// let maSive = ['desktop', 'mobile', 'Kit']
-// for (let item of maSive){
-//     console.log (item);
-// }
+let maSive = ['desktop', 'mobile', 'Kit']
+for (let item of maSive){
+    console.log (item);
+}
 
 
-//let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89]
-//for(let i=0; i<numbers.length; i++){
-//if(numbers.length>5){
-//console.log (numbers[i]);
-//}
+let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89]
+for(let i=0; i<numbers.length; i++){
+if(numbers.length>5){
+console.log (numbers[i]);
+}
 
 
-// let array = ["html", "css", "bootstrap", "javascript","python"];
-//   for(let i=0; i<array.length; i++){
-//       if(array[i].length>5 && array[i].includes('ava')){
-//           console.log(array[i]);
-//       }
-//   }
+let array = ["html", "css", "bootstrap", "javascript","python"];
+  for(let i=0; i<array.length; i++){
+      if(array[i].length>5 && array[i].includes('ava')){
+          console.log(array[i]);
+      }
+  }
 
 
 
@@ -43,4 +43,5 @@ let user = {
   if
     (user.studentstatus || user.age < 25);{
     console.log('hello world');
+    }
   }
